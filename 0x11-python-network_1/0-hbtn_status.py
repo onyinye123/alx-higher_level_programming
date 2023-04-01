7c598da0189a8c952a633d81e1aae5fb5ef352fb
onyinye123 <onyinyeudoba@gmail.com>
2023-03-31 21:39:07 -0700

    executable

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A script that
+- fetches https://intranet.hbtn.io/status.
+- uses urlib package
+"""
+
+
+if __name__ == "__main__":
+    from urllib import request
+
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
